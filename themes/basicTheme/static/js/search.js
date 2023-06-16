3c10b9f95236d5ff28f149dc3cd24ab2e0622f8b
mutonaothenton <mutonaothenton@gmail.com>
2023-06-16 14:12:16 -0400

    pagefind test

--- themes/basicTheme/static/js/search.js
+++ themes/basicTheme/static/js/search.js
@@ -1,110 +1,65 @@
-const lunr = require("lunr");
-
-const MIN_SEARCH_LENGTH = 3;
-
-const searchInput = $("#search-input");
-const searchResultsMessage = $("#search-results-message");
-const searchResultsCount = $("#search-results-count");
-const searchResultsList = $(".search-results");
-const searchResults = searchResultsList.children();
-
-let searchIndex;
-
-function showMessage(message) {
-  if (message) {
-    searchResults.hide();
-    searchResultsCount.hide();
-    searchResultsMessage.show();
-    searchResultsMessage.text(message);
-  } else {
-    searchResultsMessage.hide();
-  }
+var searchElem = document.getElementById("search-input");
+var posts;
+function loadSearch() {
+  // call the index.json file from server by http get request
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        if (data) {
+          posts = data.items; // load json data
+        }
+      } else {
+        console.log(xhr.responseText);
+      }
+    }
+  };
+  xhr.open("GET", "../index.json");
+  xhr.send();
 }
-
-function getInitialSearchTerm() {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get("q") || "";
-}
-
-function onSearch(term) {
-  if (term.length < MIN_SEARCH_LENGTH) {
-    searchResultsCount.hide();
-    showMessage(
-      `Enter a search term (of at least ${MIN_SEARCH_LENGTH} characters) to search`
-    );
-    return;
-  }
-  showMessage("");
-
-  // do search
-  const results = searchIndex.search(term).map((match) => match.ref);
-
-  if (!results.length) {
-    showMessage("No results found");
-    return;
-  }
-
-  // Show the results
-  searchResults.each(function () {
-    const el = $(this);
-    if (results.includes(this.dataset.id)) {
-      el.css("display", "flex");
+loadSearch(); // call loadsearch to load the json file
+function showSearchResults() {
+  var query = searchElem.value || ""; // get the value from input
+  var searchString = query.replace(/[^\w\s]/gi, ""); // clear white spaces
+  var target = document.getElementById("list"); // target the ul list to render the results
+  var postsByTitle = posts.reduce((acc, curr) => {
+    // map lunr search index to your articles
+    acc[curr.title] = curr;
+    return acc;
+  }, {});
+  // build lunr index file
+  var index = lunr(function () {
+    this.ref("title");
+    this.field("content");
+    posts.forEach(function (doc) {
+      this.add(doc);
+    }, this);
+  });
+  // search in lunr index
+  if (searchString && searchString != "") {
+    var matches = index.search(searchString);
+    var matchPosts = [];
+    matches.forEach((m) => {
+      matchPosts.push(postsByTitle[m.ref]);
+    });
+    if (matchPosts.length > 0) {
+      // match found with input text and lunr index
+      target.innerHTML = matchPosts
+        .map(function (p) {
+          if (p != undefined) {
+            return `<li>
+                        ${p.date} -
+                        <a href="${p.url}"> ${p.title}</a>
+                        </li>`;
+          }
+        })
+        .join("");
     } else {
-      el.css("display", "none");
+      // if no results found, then render a general message
+      target.innerHTML = `<br><h2 style="text-align:center">No search results found</h2>`;
     }
-  });
-
-  // Sort results by accuracy
-  searchResults
-    .sort(function (a, b) {
-      const aOrder = results.indexOf(a.dataset.id);
-      const bOrder = results.indexOf(b.dataset.id);
-      if (aOrder < bOrder) {
-        return -1;
-      }
-      if (aOrder > bOrder) {
-        return 1;
-      }
-      return 0;
-    })
-    .appendTo(searchResultsList);
-
-  // Show number of results
-  searchResultsCount.text(`Found ${results.length} results`);
-  searchResultsCount.show();
+  } else {
+    target.innerHTML = "";
+  }
 }
-
-$(document).ready(function () {
-  searchInput.prop("disabled", true);
-  const initialSearchTerm = getInitialSearchTerm();
-  searchInput.val(initialSearchTerm);
-
-  $.getJSON("/search/index.json", function (data) {
-    // populate the index
-    searchIndex = lunr(function () {
-      this.ref("id");
-      this.field("title");
-      this.field("content");
-
-      data.forEach(function (page) {
-        this.add(page);
-      }, this);
-    });
-
-    // add event handler
-    searchInput.on("input", function (event) {
-      onSearch(event.target.value);
-    });
-
-    // re-enable the input
-    searchInput.prop("disabled", false);
-
-    // Do an initial search
-    onSearch(searchInput.val());
-
-    // Scroll to the title if we landed on the page with a search term
-    if (initialSearchTerm) {
-      document.getElementsByTagName("h1")[0].scrollIntoView();
-    }
-  });
-});
